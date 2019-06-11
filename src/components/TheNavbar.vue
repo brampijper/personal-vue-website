d91b4749388deb1795321bfaca82d6215dd71388
brampijper <brampijper@gmail.com>
2019-06-11 14:38:05 +0200

    Implemented global variables for sass in webpack

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Content for the navbar! ;)</h1>
+  <div class="navbar-container">
+    <h1>This is content for the header</h1>
+  </div>
 </template>
+
+<style lang="scss">
+.navbar-container {
+  width: 100%;
+  background-color: $base-color;
+
+  h1 {
+    font-size: 60px;
+    color: $accent-color;
+  }
+}
+</style>
