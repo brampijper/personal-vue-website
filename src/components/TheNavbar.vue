25d706cd977d99358f09efba54dc5ce232a7f7c5
brampijper <brampijper@gmail.com>
2019-06-11 21:02:58 +0200

    implemented a simple header

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="navbar-container">
-    <h1>This is content for the header</h1>
+    <h1>Bram's Projects</h1>
   </div>
 </template>
 
 <style lang="scss">
 .navbar-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
+  height: 60px;
   background-color: $base-color;
 
   h1 {
-    font-size: 60px;
-    color: $accent-color;
+    color: $font-color;
+    font-size: 25px;
+    font-weight: 300;
+    max-width: 50%;
   }
 }
 </style>
