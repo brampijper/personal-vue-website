a777f319938342d6ba9b0679b95ee993b97996ff
brampijper <brampijper@gmail.com>
2019-06-25 13:24:17 +0200

    build the menu bar for desktop browsers

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="navbar-container">
-    <h1>Bram's Projects</h1>
+    <ul class="menu-container">
+      <li><a href="#">HOME </a></li>
+      <li>|</li>
+      <li><a href="#">BLOG </a></li>
+    </ul>
+    <div id="borderDiv"></div>
   </div>
 </template>
 
 <style lang="scss">
-.navbar-container {
+.menu-container {
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 60px;
-  background-color: $base-color;
+  height: 150px;
 
-  h1 {
-    color: $font-color;
-    font-size: 25px;
-    font-weight: 300;
-    max-width: 50%;
+  li {
+    margin: 20px;
+    display: inline;
   }
+}
+
+#borderDiv {
+  margin: 0 auto;
+  width: 80%;
+  border-bottom: 2px solid rgb(212, 212, 212);
 }
 </style>
