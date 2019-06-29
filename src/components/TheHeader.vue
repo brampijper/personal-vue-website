908bab9abb9505b5e515e12c7ac1d72244424bb7
brampijper <brampijper@gmail.com>
2019-06-29 10:02:26 +0200

    fixed responsiveness for mobile browsers

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -7,23 +7,29 @@
 <style lang="scss">
 .header-container {
   background-color: $base-color;
-  height: 82.9vh;
+  height: 71.8vh;
   text-align: center;
-}
-div > h1 {
-  margin-top: -9.2vw;
-  position: absolute;
-  font-size: 16vw;
-  color: #1f2726;
-  letter-spacing: 0.3em;
-  left: 0;
-  right: 0;
+
+  h1 {
+    margin-top: -9.2vw;
+    position: absolute;
+    font-size: 16vw;
+    color: #1f2726;
+    letter-spacing: 0.3em;
+    left: 0;
+    right: 0;
+  }
 }
 
+@media screen and (max-width: 700px) {
+  .header-container {
+    height: 77.4vh;
+  }
+}
 @media screen and (min-width: 1200px) {
-  div > h1 {
-    font-size: 250px;
-    margin-top: -148px;
+  .header-container > h1 {
+    font-size: 200px;
+    margin-top: -117px;
   }
 }
 </style>
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar-container">
     <ul class="menu-container">
       <li><a href="#">HOME </a></li>
-      <li>-</li>
+      <li>| L\O\G\O |</li>
       <li><a href="#">BLOG </a></li>
     </ul>
     <div id="borderDiv"></div>
@@ -10,22 +10,34 @@
 </template>
 
 <style lang="scss">
-.menu-container {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  height: 17vh;
+.navbar-container {
+  .menu-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 20vh;
+    margin-top: 50px;
 
-  li {
-    margin: 20px;
-    display: inline;
+    li {
+      margin: 20px;
+      display: inline;
+    }
+  }
+
+  #borderDiv {
+    margin: 0 auto;
+    width: 80%;
+    border-bottom: 1px solid rgb(107, 107, 107);
+    opacity: 0.5;
+    margin-top: -101px;
+    margin-bottom: 150px;
   }
 }
 
-#borderDiv {
-  margin: 0 auto;
-  width: 90%;
-  border-bottom: 1px solid rgb(107, 107, 107);
-  opacity: 0.5;
+@media screen and (max-width: 700px) {
+  .navbar-container > #borderDiv {
+    margin-top: -120px;
+    margin-bottom: 100px;
+  }
 }
 </style>
