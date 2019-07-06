c1e97cbf15c35f1142d57c05c5969addf6119167
brampijper <brampijper@gmail.com>
2019-07-06 16:08:12 +0200

    Created a seperate component for the expand and collapse animation

---
+++ src/components/ExtendTransition.vue
@@ -0,0 +1,25 @@
+<template>
+  <transition name="extend" mode="out-in">
+    <slot></slot>
+  </transition>
+</template>
+
+<style lang="scss">
+.extend-enter-active,
+.extend-leave-active {
+  transition: 0.9s;
+}
+
+.extend-enter-to,
+.extend-leave {
+  height: 100vh;
+}
+
+.extend-leave-to {
+  height: calc(20px + 1vw);
+}
+
+.extend-enter {
+  height: calc(40px + 3vw);
+}
+</style>
