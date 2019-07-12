9a18a14f3def3410e0901c6ef566532b2a860c1c
brampijper <brampijper@gmail.com>
2019-07-12 19:24:35 +0200

    added folder for all the transition components

---
+++ src/components/transitions/ExtendTransition.vue
@@ -0,0 +1,44 @@
+<template>
+  <transition v-bind="$attrs" name="extend">
+    <div v-show="extended" class="extend-transition-wrapper">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    extended: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.extend-transition-wrapper {
+  height: 100vh;
+  will-change: height;
+  position: relative;
+}
+
+.extend-enter-active,
+.extend-leave-active {
+  transition: 1.5s ease-out;
+}
+
+.extend-enter-to,
+.extend-leave {
+  height: 100vh;
+}
+
+.extend-leave-to {
+  height: calc(20px + 1vw);
+}
+
+.extend-enter {
+  height: calc(40px + 3vw);
+}
+</style>
