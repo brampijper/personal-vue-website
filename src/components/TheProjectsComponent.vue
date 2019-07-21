184ecb9cdd8667fb48be5ff0b61932ba289f97f4
brampijper <brampijper@gmail.com>
2019-07-21 16:43:05 +0200

    Every project card will now display a link where you can visit the website

--- src/components/TheProjectsComponent.vue
+++ src/components/TheProjectsComponent.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-      <ul>
-        <li v-for="project in testData" :key="project.id">
-          <p>{{ project.full_name}} </p>
-        </li>
-      </ul>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <div class="card-container">
+          <p>{{ project.full_name }}</p>
+          <a :href="project.pages" target="_blank"> View Website </a>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -13,27 +16,32 @@ export default {
   data() {
     return {
       VUE_APP_OCTOKIT: process.env.VUE_APP_OCTOKIT_VAR,
-      testData: []
+      projects: [],
+      pagesBaseUrl: "https://brampijper.github.io/"
     };
   },
   mounted() {
     const octokit = Octokit({
       auth: this.VUE_APP_OCTOKIT,
-      userAgent: 'brampijper',
+      userAgent: "brampijper",
       log: console
-    })
-    
-    octokit.repos.listForUser({
-      username: 'brampijper'
-    }).then(({data, headers, status}) => {
-      data.filter(repo => {
-        console.log(repo)
-        if(!repo.archived && repo.has_pages) {
-          this.testData.push(repo)
-        }
-      })
-    })
+    });
 
+    octokit.repos
+      .listForUser({
+        username: "brampijper"
+      })
+      .then(({ data }) => {
+        data.filter(project => {
+          if (!project.archived && project.has_pages) {
+            let myLength = project.full_name.length;
+            project.pages =
+              this.pagesBaseUrl + project.full_name.substring(11, myLength);
+            this.projects.push(project);
+          }
+          console.log(this.projects);
+        });
+      });
   }
 };
 </script>
@@ -41,28 +49,38 @@ export default {
 ul {
   margin-top: 150px;
   position: absolute;
-  text-align:center;
+  text-align: center;
+  width: 100vw;
+  max-height: 100%;
 
   li {
     display: inline-flex;
     width: 250px;
+    max-height: 300px;
     min-height: 250px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
     color: black;
     background-color: rgba($color: #ffffff, $alpha: 0.4);
     border-radius: 2px;
-    margin: 5px;
+    margin: 10px;
     border-top: 5px solid aquamarine;
 
-    p {
-      width:100%;
+    .card-container {
+      display: grid;
+      margin: 0 auto;
+      grid-template-rows: 80% 20% auto;
+      margin-bottom: 10px;
+
+      a {
+        align-self: end;
+      }
     }
   }
 }
 
 li:hover {
-    box-shadow: 0 14px 28px rgba(148, 106, 106, 0.15), 0 10px 10px rgba(0, 0, 0, 0.12);
+  box-shadow: 0 14px 28px rgba(148, 106, 106, 0.15),
+    0 10px 10px rgba(0, 0, 0, 0.12);
 }
-
 </style>
