c83f6c1569d431b0fdb033ff421c6de97b8f675c
brampijper <brampijper@gmail.com>
2019-07-22 21:17:32 +0200

    Emitting value from child component to show and hide the projects component depending if the topbar is extended

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div>
-    <TheProjectsComponent />
-    <AboutMeComponent />
+    <FadeTransition  :extended="!onExtend">
+      <TheProjectsComponent v-show="!onExtend" />
+    </FadeTransition>
+    <AboutMeComponent @clicked="onExtending" />
   </div>
 </template>
 
 <script>
+import FadeTransition from "./components/transitions/FadeTransition.vue"
 import AboutMeComponent from "./components/AboutMeComponent.vue";
 import TheProjectsComponent from "./components/TheProjectsComponent.vue";
 
 export default {
   components: {
+    FadeTransition,
     AboutMeComponent,
     TheProjectsComponent
+  },
+  data() {
+    return {
+      onExtend: false
+    }
+  },
+  
+  methods: {
+    onExtending(value) {
+      this.onExtend = value;
+    }
   }
 };
 </script>
