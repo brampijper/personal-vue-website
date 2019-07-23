29f295060b62ef7c59594f347b258b76b89e9a63
brampijper <brampijper@gmail.com>
2019-07-23 22:02:18 +0200

    Removed standard margin from the webpage and added styling for the body

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div>
-    <FadeTransition  :extended="!onExtend">
+    <FadeTransition :extended="!onExtend">
       <TheProjectsComponent v-show="!onExtend" />
     </FadeTransition>
-    <AboutMeComponent @clicked="onExtending" />
+    <TheHeader @clicked="onExtending" />
   </div>
 </template>
 
 <script>
-import FadeTransition from "./components/transitions/FadeTransition.vue"
-import AboutMeComponent from "./components/AboutMeComponent.vue";
+import FadeTransition from "./components/transitions/FadeTransition.vue";
+import TheHeader from "./components/TheHeader.vue";
 import TheProjectsComponent from "./components/TheProjectsComponent.vue";
 
 export default {
   components: {
     FadeTransition,
-    AboutMeComponent,
+    TheHeader,
     TheProjectsComponent
   },
   data() {
     return {
       onExtend: false
-    }
+    };
   },
-  
+
   methods: {
     onExtending(value) {
       this.onExtend = value;
@@ -31,3 +31,20 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+* {
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+  font-family: "Roboto", sans-serif;
+  list-style-type: none;
+  font-weight: 300;
+}
+
+body {
+  background-color: rgb(245, 245, 245);
+  height: 100%;
+  overflow-x: hidden;
+}
+</style>
