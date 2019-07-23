5d96f5e8a990b671a679096412735d2451a78ded
brampijper <brampijper@gmail.com>
2019-07-23 22:13:25 +0200

    Refactored this component to handle all the logic for the dumb components

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
-    <TheHeaderTopBar v-show="!extended" @click.native="onClick">
+    <TheHeaderTopBar
+      v-show="!extended"
+      @click.native="
+        onClick();
+        pickTagLines();
+      "
+    >
     </TheHeaderTopBar>
 
     <ExtendTransition :extended="extended" mode="out-in" appear>
       <TheHeaderExtended />
 
-      <FadeTransition :extended="extended" :style="{ zIndex: '2' }" appear>
-        <TheHeaderExtendedButton @click.native="onClick" />
-        <TheHeaderExtendedCards />
+      <FadeTransition
+        :extended="extended"
+        :style="{
+          zIndex: '2',
+          height: '100vh'
+        }"
+        appear
+      >
+        <div class="extended-header-wrapper">
+          <TheHeaderExtendedButton
+            @click.native="
+              onClick();
+              resetTagLines();
+            "
+          />
+          <TheHeaderExtendedIntro class="item" />
+          <TheHeaderExtendedCards :tag-lines="tagLines" class="item" />
+        </div>
       </FadeTransition>
     </ExtendTransition>
   </div>
@@ -21,6 +42,7 @@ import ExtendTransition from "./transitions/ExtendTransition.vue";
 import FadeTransition from "./transitions/FadeTransition.vue";
 import TheHeaderExtendedCards from "./TheHeaderExtendedCards.vue";
 import TheHeaderExtendedButton from "./TheHeaderExtendedButton.vue";
+import TheHeaderExtendedIntro from "./TheHeaderExtendedIntro.vue";
 
 export default {
   components: {
@@ -29,20 +51,62 @@ export default {
     ExtendTransition,
     FadeTransition,
     TheHeaderExtendedCards,
-    TheHeaderExtendedButton
+    TheHeaderExtendedButton,
+    TheHeaderExtendedIntro
   },
   data() {
     return {
-      extended: false
+      extended: false,
+      data: [
+        "I'm an outdoor kind of guy'",
+        "I like technology",
+        "I like sailing",
+        "I like coffee",
+        "I want to become the best version of myself",
+        "Traveling == adventures",
+        "Would love to do more collaboration",
+        "Norway is the coolest country",
+        "Running for motivation",
+        "I lived in Amsterdam for 2 years",
+        "Learning front-end development",
+        "Experience in display advertising",
+        "making animations with Greensock.js",
+        "Often found hiking",
+        "Flat white > cappuccino",
+        "Also a tea lover",
+        "Public transport > cars",
+        "I never stop learning"
+      ],
+      tagLines: []
     };
   },
   methods: {
     onClick() {
       this.extended = !this.extended;
       this.$emit("clicked", this.extended);
+    },
+    pickTagLines: function() {
+      for (let i = 0; i < 9; i++) {
+        let randomIndex = Math.floor(Math.random() * this.data.length);
+        let itemInArr = this.tagLines.includes(this.data[randomIndex]);
+
+        itemInArr ? i-- : this.tagLines.push(this.data[randomIndex]);
+      }
+    },
+    resetTagLines: function() {
+      this.tagLines = [];
     }
   }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.extended-header-wrapper {
+  display: grid;
+  grid-template-rows: 45% 55%;
+
+  .item {
+    height: 50vh;
+  }
+}
+</style>
