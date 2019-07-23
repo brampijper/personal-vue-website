6949acc0c9e74b569302cf012a57af1e24b2bc58
brampijper <brampijper@gmail.com>
2019-07-23 16:49:14 +0200

    The button to close the header is now a dumb component

---
+++ src/components/TheHeaderExtendedButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="icon-container">
+    <a href="#">
+      <font-awesome-icon icon="times" />
+    </a>
+  </div>
+</template>
+
+<style lang="scss">
+.icon-container {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0);
+  right: 0;
+  overflow: hidden;
+  z-index: 2;
+
+  svg {
+    padding: 35px 50px 0px 0px;
+    font-size: 55px;
+    color: black;
+    transition: all 0.6s;
+    transform: rotate(0deg);
+    transform-origin: 25% 75%;
+  }
+
+  svg:hover {
+    color: white;
+    transform: rotate(90deg);
+  }
+}
+</style>
