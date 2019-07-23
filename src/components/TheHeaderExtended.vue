a3e0219b98e7ac28a23c63261a5b92c7a0ee2870
brampijper <brampijper@gmail.com>
2019-07-23 16:48:03 +0200

    converted the extending of the header into a seperate component

---
+++ src/components/TheHeaderExtended.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-container"></div>
+</template>
+
+<style lang="scss">
+.main-container {
+  position: absolute;
+  background-color: aquamarine;
+  overflow-x: hidden;
+  overflow-y: auto;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
