8ba357804472dde38fe3bd530edc890f1f968c9c
Bram <brampijper@gmail.com>
2019-10-11 16:22:40 +0200

    sepearted webpack builds between production and development files

---
+++ build-utils/webpack.dev.js
@@ -0,0 +1,21 @@
+const config = {
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
+
+module.exports = config;
