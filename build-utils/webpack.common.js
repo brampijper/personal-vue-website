80254aecb8dce0f2c202a0bdc3f0b8cbc9c03e40
Bram <brampijper@gmail.com>
2019-10-11 16:22:40 +0200

    sepearted webpack builds between production and development files

---
+++ build-utils/webpack.common.js
@@ -0,0 +1,45 @@
+const commonPaths = require('./common-paths');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+let devConfig = {};
+devConfig.prod = require("../dev.env");
+
+const config = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: commonPaths.outputPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: "index.html",
+        }),
+        new VueLoaderPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": JSON.stringify(devConfig.prod)
+        })
+    ]
+};
+
+module.exports = config;
