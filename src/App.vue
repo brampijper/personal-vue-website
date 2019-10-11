d4d5479bbcdfd5b5156ae66e6a1553c2e17f4bab
Bram <brampijper@gmail.com>
2019-10-11 21:13:14 +0200

    Removed fadetransition and implemented logic for making api call to Github for retrieving all my personal projects

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,76 @@
 <template>
   <div>
-    <FadeTransition :extended="!onExtend">
-      <TheProjectsComponent v-show="!onExtend" />
-    </FadeTransition>
     <TheHeader @clicked="onExtending" />
+    <AppCard
+      v-show="!extended"
+      :cards="githubProjects"
+      class="project-cards"
+      card-size="medium">
+    </AppCard>
+
   </div>
 </template>
 
 <script>
-import FadeTransition from "./components/transitions/FadeTransition.vue";
+const Octokit = require("@octokit/rest");
+
 import TheHeader from "./components/TheHeader.vue";
-import TheProjectsComponent from "./components/TheProjectsComponent.vue";
+import AppCard from "./components/interface/AppCard.vue";
 
 export default {
   components: {
-    FadeTransition,
     TheHeader,
-    TheProjectsComponent
+    AppCard
+
   },
   data() {
     return {
-      onExtend: false
+      extended: false,
+      VUE_APP_OCTOKIT: process.env.VUE_APP_OCTOKIT_VAR,
+      githubProjects: [],
+      githubPageURL: "https://brampijper.github.io/"
     };
   },
+  created() {
+    const octokit = Octokit({
+      auth: this.VUE_APP_OCTOKIT,
+      userAgent: "brampijper",
+      Accept: "application/vnd.github.16.28.4.raw"
+    });
 
+    octokit.repos
+      .listForUser({
+        username: "brampijper"
+      })
+      .then(({ data }) => {
+        data.filter(project => {
+          if (!project.archived && project.has_pages) {
+            this.convertURL(project)
+            this.githubProjects.push(project);
+          };
+        });
+      });
+  },
   methods: {
     onExtending(value) {
-      this.onExtend = value;
+      this.extended = value;
+    },
+    convertURL(project) {
+      project.url = this.githubPageURL + project.full_name.substring(11, project.full_name.length);
+      return project.url;
     }
   }
 };
 </script>
 
 <style lang="scss">
+//base font
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap');
+
+$base-color: #3498db;
+$secondary-color: #F62A00;
+$font-color: #F1F3CE; 
+
 * {
   margin: 0px;
   padding: 0px;
@@ -46,5 +84,19 @@ body {
   background-color: rgb(245, 245, 245);
   height: 100%;
   overflow-x: hidden;
+}
+
+a {
+    transition: all 0.5s;
+}
+
+a:hover {
+    color: black; 
+}
+
+.project-cards {
+  position:fixed;
+  top:0;
+  margin-top:100px;
 }
 </style>
