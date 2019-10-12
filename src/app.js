24092ea11a5c8b3ac2f562ad37f48e1fc38ee439
Bram <brampijper@gmail.com>
2019-10-12 20:52:44 +0200

    imported nesessary icons from the fortawesome library

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faAt } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faTimes);
+library.add(faTimes, faLinkedin, faGithub, faAt);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
