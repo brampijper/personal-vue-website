3355b4d0a797b6b7f8f18f61f1a859c42d099eab
Bram <brampijper@gmail.com>
2019-10-13 10:32:15 +0200

    Created a component for displaying a loading spinner

---
+++ src/components/interface/AppIconLoading.vue
@@ -0,0 +1,38 @@
+<template>
+    <font-awesome-icon
+        :class="loading ? '' : 'paused'"
+        size="6x"
+        icon="spinner" />
+</template>
+
+<script>
+export default {
+    props: {
+        loading: { 
+            required: true,
+            type: Boolean
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+svg {
+    z-index:2;
+    position: fixed;
+    top: 30%;
+    left: 50%;
+    animation: rotation 2s linear infinite;
+
+}
+
+.paused {
+    animation-play-state: paused !important;
+    display:none;
+}
+
+@keyframes rotation {
+    0% { -webkit-transform: rotate(0deg)}
+    100% {-webkit-transform: rotate(359deg); }
+}
+</style>
