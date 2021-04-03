06dc1f9a1ec15f283408c4c2e0ef2767853c0297
Bram <brampijper@gmail.com>
2021-04-03 19:12:52 +0200

    added static mode to the bundle analyzer plugin so the netlify build will succeed

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,46 +1,40 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 const config = {
-    devtool: 'source-map',
-    mode: 'production',
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({}), 
-            new OptimizeCSSAssetsPlugin({})
-        ],
-        // runtimeChunk: false,
-        // splitChunks: {
-        //     cacheGroups: {
-        //         default: false,
-        //         commons: {
-        //             test: /[\\/]node_modules[\\/]/,
-        //             name: 'vendor_app',
-        //             chunks: 'all',
-        //             minChunks: 2
-        //         }
-        //     }
-        // }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({}),
-        new BundleAnalyzerPlugin()
+  devtool: "source-map",
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    // runtimeChunk: false,
+    // splitChunks: {
+    //     cacheGroups: {
+    //         default: false,
+    //         commons: {
+    //             test: /[\\/]node_modules[\\/]/,
+    //             name: 'vendor_app',
+    //             chunks: 'all',
+    //             minChunks: 2
+    //         }
+    //     }
+    // }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({}),
+    new BundleAnalyzerPlugin({ analyzerMode: "static" })
+  ]
 };
 
 module.exports = config;
