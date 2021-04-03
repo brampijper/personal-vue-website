e6ceafb33610edbf790a120baeeb37b47dd6419f
Bram <brampijper@gmail.com>
2021-04-03 19:12:52 +0200

    added static mode to the bundle analyzer plugin so the netlify build will succeed

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,21 +1,17 @@
 const config = {
-    mode: "development",
-    devtool: "eval-source-map",
-    devServer: {
-        contentBase: './dist'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    }
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
 };
 
 module.exports = config;
