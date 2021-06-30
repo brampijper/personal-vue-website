8519bded04afba0732e5e8b462dcb5c022bbe91f
Bram <brampijper@gmail.com>
2021-06-30 19:30:09 +0200

    implemented updated syntax for fontawesome

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import Vue, { createApp } from "vue";
 import App from "./App.vue";
+
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faTimes,
   faAt,
   faSpinner,
-  faEye
+  faEye,
 } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
