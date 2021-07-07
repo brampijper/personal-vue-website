2a0901068bb063db6151c04a8a46dc7ce6a820f7
Bram <brampijper@gmail.com>
2021-07-07 13:36:27 +0200

    resolved ES lint error by acessing method from Object prototype instead of the target object

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const commonConfig = require("./build-utils/webpack.common");
 const { merge } = require("webpack-merge");
 
-module.exports = env => {
+module.exports = (env) => {
   if (!env) {
     throw new Error(
       "You must pass an --env.env flag into your build for webpack to work!"
     );
   }
-  const envString = env.hasOwnProperty('dev') ? 'dev' : 'prod';
+  const envString = Object.prototype.hasOwnProperty.call(env, "dev")
+    ? "dev"
+    : "prod";
   const envConfig = require(`./build-utils/webpack.${envString}.js`);
 
   return merge(commonConfig, envConfig);
