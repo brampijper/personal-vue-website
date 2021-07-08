029eb15c764f8b045b14e93b3091b358bce9e23b
Bram <brampijper@gmail.com>
2021-07-08 15:33:55 +0200

    props and script tag are no longer needed, removed unused css code

--- src/components/transitions/ExtendTransition.vue
+++ src/components/transitions/ExtendTransition.vue
@@ -1,47 +1,25 @@
 <template>
-  <transition name="extend">
-    <div v-if="displayAboutMe" class="extend-transition-wrapper">
-      <slot></slot>
-    </div>
+  <transition name="extend" mode="out-in">
+    <slot></slot>
   </transition>
 </template>
 
-<script>
-export default {
-  props: {
-    displayAboutMe: {
-      type: Boolean,
-      default: true,
-    },
-  },
-};
-</script>
-
 <style lang="scss">
-.extend-transition-wrapper {
-  background-color: aquamarine;
-  max-height: 100vh;
-  height: 100vh;
-  will-change: height;
-  position: relative;
-  overflow: hidden;
-}
-
 .extend-enter-active,
 .extend-leave-active {
-  transition: 0.9s ease-in-out;
+  transition: max-height 0.9s ease-in-out;
 }
 
 .extend-enter-to,
-.extend-leave {
-  height: 100vh;
+.extend-leave-from {
+  max-height: 100vh;
 }
 
 .extend-leave-to {
-  height: 0;
+  max-height: 0;
 }
 
 .extend-enter {
-  height: calc(40px + 4vh);
+  max-height: calc(40px + 4vh);
 }
 </style>
