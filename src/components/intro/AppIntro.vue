b4da0e98d21eb0ac5b2e6a820478404be3292190
Bram <brampijper@gmail.com>
2021-07-12 20:40:13 +0200

    added class based animation toggle

--- src/components/intro/AppIntro.vue
+++ src/components/intro/AppIntro.vue
@@ -1,34 +1,49 @@
 <template>
-  <div class="header__content">
-    <div class="content__wrap">
-      <IntroTitle />
-      <PrimaryButton
-        class="content__button"
-        button-size="big"
-        :on-click="startTransition"
-      >
-        View Projects
-      </PrimaryButton>
-      <IntroText />
-      <IntroSocialIcons />
+  <div
+    :class="`container ${showIntro}`"
+    v-on="!showIntro ? { click: startTransition } : {}"
+  >
+    <div class="header__content">
+      <FadeTransition>
+        <div v-if="showIntro" class="content__wrap">
+          <IntroTitle />
+          <PrimaryButton
+            class="content__button"
+            button-size="big"
+            :on-click="startTransition"
+          >
+            View Projects
+          </PrimaryButton>
+          <IntroText />
+          <IntroSocialIcons />
+        </div>
+      </FadeTransition>
+      <!-- <h2 v-if="!showIntro">About Bram</h2> -->
     </div>
   </div>
 </template>
 
 <script>
-import IntroSocialIcons from "./introSocialIcons.vue";
+import IntroSocialIcons from "./IntroSocialIcons.vue";
 import IntroText from "./introText.vue";
 import IntroTitle from "./introTitle.vue";
 import PrimaryButton from "../ui/AppButton.vue";
-// import FadeTransition from "../transitions/FadeTransition.vue";
+import FadeTransition from "../transitions/FadeTransition.vue";
 
 export default {
   components: {
-    // FadeTransition,
+    FadeTransition,
     IntroSocialIcons,
     IntroTitle,
     IntroText,
     PrimaryButton,
+  },
+  props: {
+    showIntro: {
+      type: String,
+      required: true,
+      default: "show",
+    },
   },
   emits: ["toggleShowIntro"],
   setup(props, { emit }) {
@@ -43,30 +58,37 @@ export default {
 };
 </script>
 <style scoped lang="scss">
+.container.show {
+  transition: max-height 1s ease-in-out;
+  max-height: 100vh;
+  background-color: aquamarine;
+}
+
+.container {
+  transition: max-height 1s ease-in-out;
+  max-height: 5vh;
+  background-color: aquamarine;
+}
+
 .header__content {
   height: 100vh;
   display: flex;
-  background-color: aquamarine;
   will-change: height;
   overflow: hidden;
-}
-
-.content__wrap {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: auto;
-  align-items: center;
-  justify-items: start;
-  height: 100%;
-  margin: 0 auto;
-  width: max-content;
-  height: max-content;
-  align-self: center;
-  row-gap: 6rem;
-}
-
-.content__button {
-  margin-top: 15px;
+  max-height: 100vh;
+  .content__wrap {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    align-items: center;
+    justify-items: start;
+    height: 100%;
+    margin: 0 auto;
+    width: max-content;
+    height: max-content;
+    align-self: center;
+    row-gap: 6rem;
+  }
 }
 
 @media (max-width: 1150px) {
