fb01d81ecf6671afbd3946e4cce6ea10152988aa
Bram <brampijper@gmail.com>
2021-07-12 09:58:23 +0200

    updated max-height values to see what works best

--- src/components/transitions/ExtendTransition.vue
+++ src/components/transitions/ExtendTransition.vue
@@ -10,17 +10,16 @@
   transition: max-height 0.9s ease-in-out;
 }
 
+.extend-enter-from {
+  max-height: calc(40px + 4vh);
+}
+
+.extend-leave-to {
+  max-height: calc(15px + 4vh);
+}
+
 .extend-enter-to,
 .extend-leave-from {
   max-height: 100vh;
-}
-
-.extend-leave-to,
-.extend-enter-from {
-  max-height: 0;
-}
-
-.extend-enter {
-  max-height: calc(40px + 4vh);
 }
 </style>
