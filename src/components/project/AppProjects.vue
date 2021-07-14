f81bb42a1d1b47ba4bc75391b0c9fcadf52a38ac
Bram <brampijper@gmail.com>
2021-07-14 14:32:38 +0200

    created component that's responsible for displaying all the projects

---
+++ src/components/project/AppProjects.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <AppIconLoading :loading="loading" />
+    <AppCard
+      v-for="(project, index) in projects"
+      :key="index"
+      :url="project.homepage"
+      :name="project.name"
+      :description="project.description"
+      card-size="medium"
+    >
+    </AppCard>
+  </div>
+</template>
+
+<script>
+import AppCard from "../ui/AppCard.vue";
+import AppIconLoading from "../ui/IconLoading.vue";
+
+export default {
+  components: {
+    AppCard,
+    AppIconLoading,
+  },
+  props: {
+    projects: {
+      type: Array,
+      required: true,
+      default() {
+        return [];
+      },
+    },
+    loading: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 4rem;
+  text-align: center;
+  width: 100vw;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  z-index: 1;
+}
+</style>
