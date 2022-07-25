876b9fe3e38d969ec08f913191885c8cc09766b8
Bram <brampijper@gmail.com>
2022-07-25 15:24:28 +0200

    started redesign card + implementing card images

--- src/components/ui/AppCard.vue
+++ src/components/ui/AppCard.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
-    <h2>{{ title }}</h2>
+    <!-- <h2>{{ title }}</h2> -->
     <div class="card-container">
       <AppIconLoading :loading="loading" />
       <div v-for="card in cards" :key="card.id" class="card">
-        <span class="card-topbar"> </span>
         <a class="card-link" :href="card.homepage" target="_blank">
-          <span class="link-spanner"> </span>
+        <span class="card-topbar"> </span>
+          <!-- <span class="link-spanner"> </span> -->
           <div :class="cardSize + ' card-content'">
-            <p class="card-title">
-              {{ card.name }}
-            </p>
-            <p>
-              {{ card.description }}
-            </p>
-            <div class="eye-holder">
-              <font-awesome-icon size="2x" icon="eye"> </font-awesome-icon>
-              <span> View Website </span>
+            <img alt="card.alt" src="../../images/coaching_header.jpg" />
+            <!-- <img :alt="card.alt" :src="card.image" /> -->
+            <div class="card-wrap">
+              <p class="card-title">
+                {{ card.name }} {{ card.date }}
+              </p>
+              <p>
+                {{ card.description }}
+              </p>
+              <ul>
+                <li v-for="technology in card.technologies" :key="technology">
+                  {{`${technology}`}}
+                </li>
+                <br/>
+              <PrimaryButton class="content__button" button-size="tiny">
+                Visit Website
+              </PrimaryButton>
+              </ul>
             </div>
           </div>
         </a>
@@ -27,9 +36,12 @@
 
 <script>
 import AppIconLoading from "../ui/IconLoading.vue";
+import PrimaryButton from "../ui/AppButton.vue";
+
 export default {
   components: {
     AppIconLoading,
+    PrimaryButton
   },
   props: {
     cards: {
@@ -73,6 +85,14 @@ export default {
   flex-wrap: wrap;
   justify-content: center;
   z-index: 1;
+  gap: 4rem;
+}
+
+.big {
+  width: 400px;
+  min-width: 300px;
+  max-height: 600px;
+  min-height: 450px;
 }
 
 .medium {
@@ -82,36 +102,73 @@ export default {
   min-height: 250px;
 }
 
-.small {
-  height: 15vh;
-  width: 25vmin;
-  display: inline-grid;
-  place-items: center;
-  justify-self: center;
-}
+// .small {
+//   height: 15vh;
+//   width: 25vmin;
+//   display: inline-grid;
+//   place-items: center;
+//   justify-self: center;
+// }
 
 .card-content {
   display: grid;
-  grid-template-rows: 10% 60% 30% auto;
-  align-items: center;
-  margin: 0px 10px 25px 10px;
-  padding: 0 15px 15px 15px;
+  grid-template-rows: 0.75fr 1fr;
+  align-items: start;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   color: black;
   background-color: rgba($color: #ffffff, $alpha: 0.4);
-  border-radius: 2px;
+  border-radius: 15px;
   letter-spacing: 1.5px;
+  row-gap: 0.7rem;
 
-  > p {
-    z-index: 2;
+  img {
+    border-radius: 15px 15px 0px 0px;
+    max-width: 100%;
+    // filter: blur(2px);
   }
+}
 
+.card-wrap {
+  margin: 0px 10px 25px 10px;
+  padding: 0 30px 10px 30px;
+  text-align:left;
+  display: flex;
+  flex-direction:column;
+  row-gap: 0.8rem;
+  
   .card-title {
     font-weight: 600;
-    padding: 5px;
-    margin-top: 25px;
   }
+
+  > p {
+    z-index: 3;
+  }
+
+  ul {
+    background-color:black;
+    border-radius: 5px;
+    padding:1rem;
+    margin-top:.5rem;
+  }
+
+  ul > li {
+    display: inline-block;
+    margin-right: .8rem;
+    margin-bottom:.4rem;
+    border: 1px solid white;
+    border-radius: 5px;
+    padding: 0.5rem;
+    font-size:.75rem;
+    color:white;
+  }
+
+  .content__button {
+    background-color: white;
+    color: black;
+    margin-top:.8rem;
+  }
+
 }
 
 .card > span {
@@ -123,40 +180,30 @@ export default {
   text-decoration: none;
 }
 
-.card {
+.card, .card-link {
   position: relative;
 }
 
-.eye-holder {
-  z-index: 2;
-  opacity: 0;
-  transition: all 1s;
-}
-
-.eye-holder span {
-  display: block;
-  font-size: 0.8rem;
-  margin-top: 7px;
-}
-
-.link-spanner {
-  position: absolute;
-  width: 94%;
-  margin: 0 auto;
-  height: 92%;
-  top: 0;
-  left: 10px;
-  z-index: 1;
-}
+// .link-spanner {
+//   position: absolute;
+//   width: 94%;
+//   margin: 0 auto;
+//   height: 92%;
+//   top: 0;
+//   left: 10px;
+//   z-index: 1;
+// }
 
 .card-topbar {
-  width: 280px;
+  width: 400px;
   display: block;
   background-color: aquamarine;
-  height: 5px;
-  margin-left: 10px;
+  height: 1px;
   transition: all 1.1s;
   position: absolute;
+  border-radius: 15px;
+  z-index: 2;
+  opacity: .75;
 }
 
 .card:hover > .card-content {
@@ -165,34 +212,14 @@ export default {
   cursor: pointer;
 }
 
-.card:hover > .card-topbar {
-  height: 265px;
-  opacity: 0.75;
+.card-link:hover > .card-topbar {
+  height: 100%;
+  opacity: 0.2;
 }
 
-.card:hover .eye-holder {
-  opacity: 1;
+.card-link:hover > .card-content img {
+  // filter: blur(0px);
+  // transition: .4s;
 }
 
-.projects.dark-mode {
-  background-color: #212121;
-  h2 {
-    color: aquamarine;
-  }
-}
-
-.dark-mode {
-  .card-content {
-    color: whitesmoke;
-    background-color: rgba($color: #ffffff, $alpha: 0.2);
-  }
-
-  .eye-holder {
-    color: aquamarine;
-  }
-
-  .card-topbar {
-    background-color: black;
-  }
-}
 </style>
