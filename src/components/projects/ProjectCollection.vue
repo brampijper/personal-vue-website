89b376acbec725f40aff9a2eb8715947bb8012d9
Bram <brampijper@gmail.com>
2022-10-28 15:27:11 +0200

    restructured html and css elements

--- src/components/projects/ProjectCollection.vue
+++ src/components/projects/ProjectCollection.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="projects">
-    <h2 class="">Projects</h2>
+  <section>
+    <h2>Projects</h2>
     <div class="project__cards">
       <AppIconLoading :loading="loading" />
-      
       <ProjectCard 
         v-for="project in projects" 
         :key="project.id"
         :project="project"
       />
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -38,21 +37,15 @@ export default {
 <style scoped lang="scss">
 @import "~rfs/scss";
 
-.projects {
+section {
   width: 75vw;
-  height: auto;
-  margin: 4rem;
   margin: 0 auto;
-}
 
-.projects h2 {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
-  sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  @include font-size(4rem);
-  font-weight: 900;
-  line-height: 1;
-  margin: 0 0 10px 0;
-  color: #eff2ed;
+  h2 {
+    @include font-size(4rem);
+    line-height: 1;
+    color: $h2-color;
+  }
 }
 
 .project__cards {
@@ -66,7 +59,7 @@ export default {
 }
 
 @media (max-width: 668px) {
-  .projects {
+  section {
     width: 95vw;
     margin: 5rem 1rem 5rem 1rem;
   }
