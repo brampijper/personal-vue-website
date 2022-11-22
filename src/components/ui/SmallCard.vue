7f5c1272a8063049b862357f884514598d53edc4
Bram <brampijper@gmail.com>
2022-11-22 12:35:50 +0100

    created styles for the highlight card

--- src/components/ui/SmallCard.vue
+++ src/components/ui/SmallCard.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="container card">
-    <div class="container__wrap">
-        <h2>Highlights</h2>
-        <ul>
-            <li>project</li>
-            <li>project</li>
-            <li>Latest blog post</li>
-        </ul>
-    </div>
+    <ul class="container__wrap">
+      <li>-- stats Ghub commits --</li>
+      <li>-- project highlight --</li>
+      <li>-- latest blog post --</li>
+    </ul>
   </div>
 </template>
 
@@ -27,19 +24,42 @@ export default {
 @import "~rfs/scss";
 
 .container {
-    border-radius: 16px;
-    padding: 2rem;
     box-sizing: border-box;
     margin: 0px;
     flex-direction: row;
+    overflow:hidden;
+}
+
+.container__wrap {
+  list-style: none;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+
+  li {
+    flex-basis: 33%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px dashed goldenrod;
+    cursor: pointer;
+    font-weight: 700;
+    font-family: $title-font-family;
+    min-height: 100px;
+  }
+
+  li:last-child {
+    border: none;
+  }
 }
 
 @media (min-width: 0px) {
-    .container {
-        -webkit-box-pack: justify;
-        justify-content: space-between;
-        width: 100%;
-    }
+  .container {
+      -webkit-box-pack: justify;
+      justify-content: space-between;
+      width: 100%;
+  }
 }
 
 @media (min-width: 990px) {
