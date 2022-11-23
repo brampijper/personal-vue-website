8376a8379f7a3b193ee50f5db5846f6da77a6d68
Bram <brampijper@gmail.com>
2022-11-23 18:36:42 +0100

    creaed footer component

---
+++ src/components/layout/AppFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer>
+    <div className="footer-wrap">
+        <IntroSocialIcons />
+    </div>
+  </footer>
+</template>
+
+<script>
+import IntroSocialIcons from '../intro/IntroSocialIcons.vue'
+export default {
+  components: {
+    IntroSocialIcons
+  },
+  data() {
+    return {}
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~rfs/scss";
+
+footer {
+    background-color: $card-bg;
+    color: $font-colors;
+    position:relative;
+    width: 100%;
+    box-shadow: rgb(0 0 0 / 14%) 0px 0px 24px 0px;
+    // border-top: 1px solid rgba(22, 22, 22, 0.2);    
+    min-height: 112px;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+}
+
+.dark {
+  background-color: $card-bg-dark;
+  color: $font-colors-dark;
+}
+
+</style>
