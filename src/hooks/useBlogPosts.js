ca76284a731bb901c785ed0646bafa697d50ccad
Bram <brampijper@gmail.com>
2023-04-16 12:35:37 +0200

    removed CORS, added better error handling

--- src/hooks/useBlogPosts.js
+++ src/hooks/useBlogPosts.js
@@ -1,16 +1,19 @@
 import Parser from 'rss-parser';
 
 export default async function useBlogPosts() {
-
-    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
     let parser = new Parser();
 
     async function getBlogPost() {
-        const feed = await parser.parseURL(CORS_PROXY + 'https://focused-galileo-c3ee18.netlify.app/rss.xml');
-
-        if(feed) {
-            return feed.items[0]
-        } else {
+        try {
+            const feed = await parser.parseURL('https://focused-galileo-c3ee18.netlify.app/rss.xml');
+            
+            if (feed && feed.items.length) {
+                const [firstBlogPost] = feed.items;
+                return firstBlogPost;
+            } else {
+                console.log("no feed returned", feed)
+            }
+        } catch (err) {
             throw new Error(err)
         }
     }
