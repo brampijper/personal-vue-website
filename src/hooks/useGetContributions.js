99336a48ffd2edd37bbc1eff7554603143bf6548
Bram <brampijper@gmail.com>
2023-04-20 20:25:44 +0200

    updated: fetching data from back-end server

--- src/hooks/useGetContributions.js
+++ src/hooks/useGetContributions.js
@@ -1,32 +1,13 @@
-export default async function useGetContributions(username, token) {
-  const headers = {
-      'Authorization': ` bearer ${token}`,
-  }
-  const body = {
-      "query": `query {
-          user(login: "${username}") {
-            name
-            contributionsCollection {
-              contributionCalendar {
-                colors
-                totalContributions
-                weeks {
-                  contributionDays {
-                    color
-                    contributionCount
-                    date
-                    weekday
-                  }
-                  firstDay
-                }
-              }
-            }
-          }
-        }`
-  }
-  const response = await fetch('https://api.github.com/graphql', { method: 'POST', body: JSON.stringify(body), headers: headers })
-  const { data } = await response.json()
-  const { 
+export default async function useGetContributions() {
+  try {
+    const response = await fetch('https://seashell-app-u77ys.ondigitalocean.app/api/stats')
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const { data } = await response.json();
+    const { 
       user: { 
         contributionsCollection: { 
           contributionCalendar: { 
@@ -34,6 +15,11 @@ export default async function useGetContributions(username, token) {
           }
         }
       }  
-  } = data
-  return totalContributions
+    } = data
+    return totalContributions;    
+  }
+
+  catch(error) {
+    console.error('Problem while fetching: ', error)
+ }
 }
