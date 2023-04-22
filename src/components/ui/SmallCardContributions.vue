f6abbb2f0bb2a3fd777e35e4261b7cd1b64edcb9
Bram <brampijper@gmail.com>
2023-04-22 19:46:52 +0200

    added: component for fetching github data

---
+++ src/components/ui/SmallCardContributions.vue
@@ -0,0 +1,43 @@
+<template>
+    <a 
+    class="card"
+    :href="state.link"
+    target="_blank" 
+    >
+
+        <AppIconLoading v-if="state.isLoading" :loading="state.isLoading" />
+        
+        <h4 v-if="!state.isLoading">
+            Github Contributions: {{ state.totalContributions }}
+        </h4>
+    </a> 
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import AppIconLoading from "./IconLoading.vue";
+import useGetContributions from '../../hooks/useGetContributions.js';
+
+export default {
+  components: {
+    AppIconLoading,
+  },
+  setup() {
+    const state = ref({ isLoading: true })
+
+    onMounted( async () => {
+      const totalContributions = await useGetContributions()
+      state.value = {
+        isLoading: false,
+        totalContributions,
+      }
+    })
+
+    return {
+      state
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
