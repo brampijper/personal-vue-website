350e34f2dd1d366e68bb844c0bf5b781f187a5c6
Bram <brampijper@gmail.com>
2023-04-22 19:47:06 +0200

    added: component for displaying my contact info

---
+++ src/components/ui/SmallCardContact.vue
@@ -0,0 +1,25 @@
+<template>
+    <a 
+    class="card"
+    href="mailto:brampijper@proton.me"
+    target="_blank">
+        <article class="card-content">
+          <h4>Opportunities | Feedback | Random ? </h4>
+          <p> Happy to connect & chat, drop a line at </p>
+        </article>
+        <span class="card__content-email">
+          brampijper@proton.me
+        </span>
+    </a>
+</template>
+
+<script>
+
+export default {
+  setup() {
+    return {}
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
