e4b4fc9d8561b977619ff4f2d5370221ba5faa71
Bram <brampijper@gmail.com>
2023-04-22 19:46:39 +0200

    added: component for fetching blog data

---
+++ src/components/ui/SmallCardBlog.vue
@@ -0,0 +1,53 @@
+<template>
+  <a  class="card"
+      :href="state.link"
+      target="_blank">
+      
+    <AppIconLoading v-if="state.isLoading" :loading="state.isLoading" />
+    
+    <article class="card-content" v-else>
+        <h4>Check out my latest blogpost</h4>
+        <p><i>{{ `"${state.title}"` }}</i></p>
+        <small><i>Written on {{ state.pubDate }}</i></small>
+    </article>
+  </a>      
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import AppIconLoading from "./IconLoading.vue";
+import useBlogPosts from '../../hooks/useBlogPosts.js';
+
+export default {
+  components: {
+    AppIconLoading,
+  },
+  setup() {
+    const state = ref({ isLoading: true })
+
+    onMounted( async () => {
+      const { firstPost, isLoading } =  await useBlogPosts();
+      const { pubDate, title, link, } = firstPost;
+      state.value = {
+        isLoading,
+        pubDate: formatDate(pubDate),
+        title,
+        link,
+      }
+    })
+
+    function formatDate(date) {
+      const dateArr = date.split(" ");
+      const removeLastTwoWords = dateArr.slice(0, -2); // removes the GMT + Time.
+      const formattedDate = removeLastTwoWords.join(" ");
+      return formattedDate;
+    }
+
+    return {
+      state
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
