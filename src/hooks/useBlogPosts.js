d312f85a561cc40581a06bf6f17224097a4a7be4
Bram <brampijper@gmail.com>
2023-04-23 17:23:03 +0200

    updated to latest rss parser v

--- src/hooks/useBlogPosts.js
+++ src/hooks/useBlogPosts.js
@@ -1,26 +1,23 @@
 import Parser from 'rss-parser';
+const parser = new Parser()
 
 export default async function useBlogPosts() {
     let state = {
         isLoading: true
     }
-    let parser = new Parser();
 
     async function fetchRSSFeed() {
-        try {
-            const feed = await parser.parseURL('https://focused-galileo-c3ee18.netlify.app/rss.xml');
-            
-            if (feed && feed.items.length) {
-                const [firstPost] = feed.items;
-                state = {...state, firstPost}; 
-                state.isLoading = false;
-            } else {
-                console.log("no feed returned", feed)
-            }
-        return state;
-        } catch (err) {
-            throw new Error(err)
+        const feed = await parser.parseURL('https://focused-galileo-c3ee18.netlify.app/rss.xml');
+        
+        if (feed && feed.items.length) {
+            const [firstPost] = feed.items;
+            state = {...state, firstPost}; 
+            state.isLoading = false;
+        } else {
+            const message = `An error has occured: ${feed}`;
+            throw new Error(message); 
         }
+        return state;
     }
-    return await fetchRSSFeed();
+    return await fetchRSSFeed()
 }
