680fa47566954022a08313069c2919479f57f725
Bram <brampijper@gmail.com>
2023-04-24 14:31:44 +0200

    created a single hook for handling api requests

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,18 @@
+export default async function useFetchData(path, param = "") {
+    const baseURL = 'https://seashell-app-u77ys.ondigitalocean.app'; //change baseurl between http://localhost:3000 (dev) / https://seashell-app-u77ys.ondigitalocean.app (prod)
+    const fullURL = `${baseURL}${path}${param}`; 
+
+    try {
+        const response = await fetch(fullURL);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();        
+        return data;
+
+    } catch(error) {
+        console.error('Problem while fetching: ', error)
+    }
+}
