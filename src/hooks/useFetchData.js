50466e01c2520c9b6b289d9a94dde88dbdbee64b
Bram <brampijper@gmail.com>
2023-04-25 21:04:59 +0200

    added: caching frontend

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,18 +1,69 @@
-export default async function useFetchData(path, param = "") {
-    const baseURL = 'https://seashell-app-u77ys.ondigitalocean.app'; //change baseurl between http://localhost:3000 (dev) / https://seashell-app-u77ys.ondigitalocean.app (prod)
-    const fullURL = `${baseURL}${path}${param}`; 
+import { reactive } from 'vue';
 
-    try {
-        const response = await fetch(fullURL);
+export function useFetchData() {
+    const state = reactive({
+        data: null,
+        version: null,
+    });
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+    async function fetchData (path, param = "") {
+        const baseURL = 'http://localhost:3000'; //change baseurl between http://localhost:3000 (dev) / https://seashell-app-u77ys.ondigitalocean.app (prod)
+        const fullURL = `${baseURL}${path}${param}`; 
+
+        if (state.data && state.version) {
+            return state.data;
         }
 
-        const data = await response.json();        
-        return data;
+        try {
+            const response = await fetch(fullURL);
+            const etag = response.headers.get('ETag');
+            console.log(etag)
 
-    } catch(error) {
-        console.error('Problem while fetching: ', error)
+            if (etag === state.version) {
+                console.log('match')
+                return state.data;
+            }
+
+            state.version = etag;
+            const data = await response.json(); 
+            state.data = data;
+            return data;
+        } catch (error) {
+            throw new Error('Failed to fetch data ', error)
+        }
     }
+
+    return { fetchData };
 }
+
+// export default async function useFetchData(path, param = "") {
+
+//     if(cachedData[path] && cachedData[path].version === version) {
+//         console.log('returning cache: ', cachedData[path.data])
+//         return Promise.resolve(cachedData[path.data]);
+//     }    
+
+//     try {
+
+
+//         const response = await fetch(fullURL);
+
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+
+//         const {data} = await response.json(); 
+//         const etag = response.headers.get('ETag'); 
+        
+//         if (etag === cachedVersion) {
+//             return cachedData;
+//         }
+
+//         cachedVersion = etag;
+//         console.log(data)     
+//         return data;
+
+//     } catch(error) {
+//         console.error('Problem while fetching: ', error)
+//     }
+// }
