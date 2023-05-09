cf8606bc21e665d33d42ac3804c19694012ef391
Bram <brampijper@gmail.com>
2023-05-09 12:18:15 +0200

    converted to async setup component

--- src/components/ui/SmallCardContributions.vue
+++ src/components/ui/SmallCardContributions.vue
@@ -1,42 +1,25 @@
 <template>
   <a class="card" href="https://github.com/brampijper" target="_blank">
-    <AppIconLoading :isLoading="state.isLoading" /> 
-    <h4 v-if="!state.isLoading">
-        Github Contributions: {{ state.totalContributions }}
+    <h4>
+        Github Contributions: {{ state }}
     </h4>
   </a> 
 </template>
 
-<script>
-import { ref, onMounted } from 'vue';
-import AppIconLoading from "./IconLoading.vue";
-import fetchAndCacheData from "../../hooks/useFetchAndCacheData";
-
-export default {
-  components: {
-    AppIconLoading,
-  },
-  setup() {
-    const state = ref({ isLoading: true })
-
-    onMounted( async () => {
-      try {
-        const data = await fetchAndCacheData("/api/stats", "?username=brampijper")
-        state.value = {
-          totalContributions: data,
-          isLoading: false
-        }
-
-      } catch (error) {
-        console.error(error)
-      }
-    })
-
-    return {
-      state,
-    }
+<script async setup>
+  import { ref, onMounted } from 'vue';
+  import AppIconLoading from "./IconLoading.vue";
+  import fetchAndCacheData from "../../hooks/useFetchAndCacheData";
+  
+  const state = ref('')
+  
+  try {
+    const totalContributions = await fetchAndCacheData("/api/stats", "?username=brampijper") // returns a string
+    state.value = totalContributions;
+    
+  } catch (err) {
+    console.log('Error while fetching data: ', err)
   }
-};
+  
+
 </script>
-<style lang="scss" scoped>
-</style>
