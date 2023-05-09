6868511788cd0989ced14b096d8dd33d7ddb61ea
Bram <brampijper@gmail.com>
2023-05-09 19:02:45 +0200

    added fallback components for handling the async components

--- src/components/ui/SmallCards.vue
+++ src/components/ui/SmallCards.vue
@@ -3,14 +3,18 @@
     <Suspense>
       <SmallCardBlog />
       <template #fallback> 
-        <div class="card">loading...</div>
+        <div class="card">
+          <span class="skeleton skeleton__text"></span>
+        </div>
       </template>
     </Suspense>
 
     <Suspense>
       <SmallCardContributions />
       <template #fallback> 
-        <div class="card">loading...</div>
+        <div class="card">
+          <span class="skeleton skeleton__text"></span> <!-- uses skeleton classes (which are non scoped) -->
+        </div>      
       </template>
     </Suspense>
 
@@ -27,34 +31,31 @@
 <style lang="scss" scoped>
 
 .small__cards {
-    display: flex;
-    flex-direction:column;
-    gap: 2.3rem;
-    box-sizing: border-box;
-    list-style: none;
-    height: 100%;
+  display: flex;
+  flex-direction:column;
+  gap: 2.3rem;
+  box-sizing: border-box;
+  list-style: none;
+  height: 100%;
 
-    .card {
-      padding: 2rem;
-      align-items:center;
-    }
+  .card {
+    padding: 2rem;
+    align-items:center;
+    flex-basis: 29%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 0.8rem;
+    cursor: pointer;
+    font-weight: 700;
+    font-family: $title-font-family;
+    min-height: 100px;
+  }
     
-    a {
-        flex-basis: 29%;
-        display:flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        gap: 0.8rem;
-        cursor: pointer;
-        font-weight: 700;
-        font-family: $title-font-family;
-        min-height: 100px;
-    }
-    
-    a:hover {
-        background-color:white;
-        color:black;
+  a:hover {
+    background-color:white;
+    color:black;
   }
 }
 
@@ -69,6 +70,10 @@
 @media (min-width: 990px) {
   .small__cards {
     flex-flow:column;
+    
+    .card {
+      min-width: 250px;
+    }
   }
 }
   
