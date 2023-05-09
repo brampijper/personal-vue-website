b14d723f946f37c61e3824a9296b90dca2eac87b
Bram <brampijper@gmail.com>
2023-05-09 19:03:22 +0200

    removed loading icon

--- src/components/ui/SmallCardContributions.vue
+++ src/components/ui/SmallCardContributions.vue
@@ -1,5 +1,9 @@
 <template>
-  <a class="card" href="https://github.com/brampijper" target="_blank">
+  <a 
+    class="card" 
+    href="https://github.com/brampijper" 
+    target="_blank"
+  >
     <h4>
         Github Contributions: {{ state }}
     </h4>
@@ -8,7 +12,6 @@
 
 <script async setup>
   import { ref, onMounted } from 'vue';
-  import AppIconLoading from "./IconLoading.vue";
   import fetchAndCacheData from "../../hooks/useFetchAndCacheData";
   
   const state = ref('')
@@ -16,7 +19,6 @@
   try {
     const totalContributions = await fetchAndCacheData("/api/stats", "?username=brampijper") // returns a string
     state.value = totalContributions;
-    
   } catch (err) {
     console.log('Error while fetching data: ', err)
   }
